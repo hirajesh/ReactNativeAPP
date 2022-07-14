9f36f3bd77857ebfc87a8cbfd79adc5676359ef5
Rajesh <35864342+hirajesh@users.noreply.github.com>
2022-07-14 17:38:18 +0530

    React native
    initial commit

---
+++ src/components/IconContainer2.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+const IconContainer = ({ iconName }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.iconContainer}>
+      {iconName === 'arrow-left' 
+        ? 
+        <Icon name={iconName} color={'white'} size={25} onPress={() => navigation.goBack()} />
+        :
+        <Icon name={iconName} color={'white'} size={25} />
+      }
+    </View>
+  )
+}
+
+export default IconContainer
+
+const styles = StyleSheet.create({
+    iconContainer: {
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black',
+        width: 55,
+        height: 55,
+        borderRadius: 15,
+    },
+})
